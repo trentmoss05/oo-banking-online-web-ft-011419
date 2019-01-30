ca3a26ad81fc22c9545309cb4a7b1fc0d8cd6f24
Trent Moss <trentmoss05@gmail.com>
2019-01-30 15:00:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,13 +17,14 @@ class Transfer
   end
 
   def execute_transaction
-    if self.sender.valid?
-      "Transaction rejected. Please check your account balance."
+    if @sender.balance < @amount
       @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    elsif @status == "complete"
+      puts "Transaction was already executed"
     else
-
-      self.sender.balance = self.sender.balance - @amount
-      self.receiver.balance = self.receiver.balance + @amount
+      @sender.balance = @sender.balance - @amount
+      @receiver.balance = @receiver.balance + @amount
       @status = "complete"
     end
   end
